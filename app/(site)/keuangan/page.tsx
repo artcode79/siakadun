c0e0883dc90688b717fa8f4d9edcda320e0fdb11
artcode79 <121704036+artcode79@users.noreply.github.com>
2023-08-04 12:34:52 +0700

    Update page.tsx

--- app/(site)/keuangan/page.tsx
+++ app/(site)/keuangan/page.tsx
@@ -13,23 +13,7 @@ import { db } from '../../../libs/firebase'
 interface KeuanganProps {}
 
 const Keuangan: React.FC<KeuanganProps> = ({}) => {
-	const [loading, setLoading] = useState(true)
-	const [error, setError] = useState(null)
-	const [data, setData] = useState(null)
-
 	
-	console.log(data)
-	console.log(error)
-	console.log(loading)
-
-	//
-	if (loading) {
-		return <div>Loading...</div>
-	}
-	if (error) {
-		return <div>Error: {error.message}</div>
-	}
-
 	return (
 		<div>
 			<h1>Keuangan</h1>
